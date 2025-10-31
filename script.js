66c450a1c2e9b44c2508152b1424a2352f6eac58
Snehitha <lakkurusnehitha@gmail.com>
2025-10-31 22:56:52 +0530

    Updated

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
 let sections = document.querySelectorAll('section');
 let navlinks = document.querySelectorAll('header nav a'); 
 
@@ -19,4 +27,27 @@ window.onscroll = () => {
     });  
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
+
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+ScrollReveal().reveal('.home-img, .services-container, .portfolio-box, .contact form', { origin: 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin: 'left' });
+ScrollReveal().reveal('.home-content p, .about-content', { origin: 'right' });
+
+const typed = new Typed('.multiple-text', {
+    strings: ['Frontend', 'Programming'],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+}); 
